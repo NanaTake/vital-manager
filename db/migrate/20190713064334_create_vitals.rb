6deb4f4cb945cdbc08f493584f1e892f7153910a
NanaTake <son_magomago@yahoo.co.jp>
2019-07-13 16:00:44 +0900

    Create vital-table

---
+++ db/migrate/20190713064334_create_vitals.rb
@@ -0,0 +1,12 @@
+class CreateVitals < ActiveRecord::Migration[5.0]
+  def change
+    create_table :vitals do |t|
+      t.date     :record_date, null: false
+      t.float    :weight
+      t.integer  :blood_pressure
+      t.integer  :pulse
+      t.string   :memo
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711085340) do
+ActiveRecord::Schema.define(version: 20190713064334) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20190711085340) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "vitals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.date     "record_date",               null: false
+    t.float    "weight",         limit: 24
+    t.integer  "blood_pressure"
+    t.integer  "pulse"
+    t.string   "memo"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
